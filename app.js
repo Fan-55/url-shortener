de059c0ab3c58a00afb35893c1e747bf38a68ce2
Fan5 <openconvenience@gmail.com>
2020-10-24 00:41:47 +0800

    feat: add routes for home and urls

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express')
-const app = express()
 const exphbs = require('express-handlebars')
+const routes = require('./routes/index')
+
+const app = express()
 const PORT = 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
 app.listen(PORT, () => console.log(`This app is listening at http://localhost:${PORT}`))
