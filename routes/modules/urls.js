c51f6963c740db5d5308757c4989f1429cf344e8
Fan5 <openconvenience@gmail.com>
2020-10-24 20:38:59 +0800

    feat: add validation function to prevent empty or space-only input on backend

--- routes/modules/urls.js
+++ routes/modules/urls.js
@@ -4,34 +4,37 @@ const Url = require('../../models/url')
 
 router.post('/', async (req, res, next) => {
   const baseUrl = process.env.BASE_URL || 'https://secret-oasis-08507.herokuapp.com/'
-  try {
-    const url = await Url.findOne(req.body).lean()
+  const originalUrl = req.body.originalUrl.trim()
+  //Error is thrown if url input (originalUrl) is empty or only contains spaces
+  if (originalUrl) {
+    try {
+      const url = await Url.findOne(req.body).lean()
 
-    if (url) {
-      const pathname = url.pathname
-      const shortenUrl = baseUrl + pathname
-      res.render('index', { shortenUrl })
-    } else {
-      const getPathname = require('../../utils/getPathname')
-      let pathname
-      let duplicateUrl
+      if (url) {
+        const pathname = url.pathname
+        const shortenUrl = baseUrl + pathname
+        res.render('index', { shortenUrl })
+      } else {
+        const getPathname = require('../../utils/getPathname')
+        let pathname
+        let duplicateUrl
 
-      do {
-        pathname = getPathname()
-        duplicateUrl = await Url.findOne({ pathname }).lean()
-      } while (duplicateUrl)
+        do {
+          pathname = getPathname()
+          duplicateUrl = await Url.findOne({ pathname }).lean()
+        } while (duplicateUrl)
 
-      Url.create({
-        originalUrl: req.body.originalUrl,
-        pathname: pathname
-      })
+        Url.create({ originalUrl, pathname })
+        const shortenUrl = baseUrl + pathname
+        res.render('index', { shortenUrl })
+      }
 
-      const shortenUrl = baseUrl + pathname
-      res.render('index', { shortenUrl })
+    } catch (err) {
+      console.log(err)
     }
-
-  } catch (err) {
-    console.log(err)
+  } else {
+    const err = new Error('Invalid input')
+    next(err)
   }
 })
 
