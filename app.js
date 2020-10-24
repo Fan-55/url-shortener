c8841b09ed944ea502fdbabb89fd04afd8f3fcc7
Fan5 <openconvenience@gmail.com>
2020-10-25 02:23:56 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes/index')
+const bodyParser = require('body-parser')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+require('./config/mongoose')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 app.use(routes)
 
 app.listen(PORT, () => console.log(`This app is listening at http://localhost:${PORT}`))
