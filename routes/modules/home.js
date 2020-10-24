f6518ffc7b93401c59f694efea1f6a8bb2bee4b7
Fan5 <openconvenience@gmail.com>
2020-10-24 13:51:15 +0800

    feat: add function of redirecting shorten url to original url

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const Url = require('../../models/url')
 
 router.get('/', (req, res, next) => {
+  res.render('index')
+})
 
+router.get('/:pathname', async (req, res, next) => {
+  try {
+    const pathname = req.params.pathname
+    const url = await Url.findOne({ pathname }).lean()
+
+    if (url) {
+      res.redirect(url.originalUrl)
+    } else {
+      const err = new Error('Invalid URL')
+      next(err)
+    }
+
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router
